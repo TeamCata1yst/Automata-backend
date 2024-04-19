364dfe599ca561373e71ac093af51b5911202520
Aaditya Kansal <aadityakansal390@gmail.com>
2024-04-19 11:04:57 +0530

    checks

--- misc/time.js
+++ misc/time.js
@@ -21,7 +21,8 @@ const totalTime = (start, now_t, init_time, hours, process) => {
                 let v = new Date(Date.parse(now_t) + process[ptr].time_req)
                 let a = v.getHours()
                 let b = v.getMinutes()
-                if( (a + b/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) && (a + b/60 >= init_time[0] + init_time[1]/60) ) { 
+                if( (a + b/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) && (a + b/60 >= init_time[0] + init_time[1]/60) && now_t.getDay() == v.getDay() && now_t.getDate() == v.getDate() ) { 
+                    process[ptr].init_time = new Date(Date.parse(now_t))
                     let val = new Date(process[ptr].time_req)
                     
                     now_t.setHours(now_t.getHours() + val.getHours())
@@ -36,11 +37,16 @@ const totalTime = (start, now_t, init_time, hours, process) => {
                     now_t.setHours(init_time[0], init_time[1])
                     if(!( a + b/60 < init_time[0] + init_time[1]/60 && a + b/60 >= 0))
                         now_t = new Date(Date.parse(now_t) + 24*60*60*1000)
-                        
+                    //left hour logic with while
+                    var left_over = process[ptr].time_req/(24*60*60*1000)
+                    while(left)
+                    
                     var n = new Date()
                     n.setHours(init_time[0] + hours[0], init_time[1] + hours[1]*60)
                     n.setDate(now_t.getDate())
                     n.setMonth(now_t.getMonth())
+                    
+                    
                     process[ptr].deadline = n
                 }
             }
