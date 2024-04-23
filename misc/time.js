763dcb0f7ee632eefce8b814bdb5731be64f4984
Aaditya Kansal <aadityakansal390@gmail.com>
2024-04-23 12:11:32 +0530

    time

--- misc/time.js
+++ misc/time.js
@@ -15,16 +15,16 @@ const totalTime = (start, now_t, init_time, hours, weekend, process) => {
                     now_t.setHours(init_time[0], init_time[1])    
                     now_t.setDate(now_t.getDate() + 1)
                 }
-                if(now_t.getHours() + now_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && now_t.getHours() + now_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) {
+                if(now_t.getHours() + now_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && now_t.getHours() + now_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) {
                     process[ptr].init_time = new Date(Date.parse(now_t))
                     var after_t = new Date(Date.parse(now_t) + process[ptr].time_req)
-                    if((after_t.getHours() + after_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && after_t.getHours() + after_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) && now_t.getDate() == after_t.getDate() && now_t.getFullYear() == after_t.getFullYear() && now_t.getMonth() == after_t.getMonth()) {
+                    if((after_t.getHours() + after_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && after_t.getHours() + after_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) && now_t.getDate() == after_t.getDate() && now_t.getFullYear() == after_t.getFullYear() && now_t.getMonth() == after_t.getMonth()) {
                         let val = process[ptr].time_req/(1000*60*60)
-                        now_t.setHours(now_t.getHours() + Math.floor(val), now_t.getMinutes() + ((val*10)%10)*60) 
+                        now_t.setHours(now_t.getHours() + Math.floor(val), now_t.getMinutes() + ((val*10)%10)*6) 
                         process[ptr].deadline = new Date(Date.parse(now_t))
                     } else {
                                             
-                        let dis = ((init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) - (now_t.getHours() + now_t.getMinutes()/60)
+                        let dis = ((init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) - (now_t.getHours() + now_t.getMinutes()/60)
                         var left_over = process[ptr].time_req/(1000*60*60) - dis
                         now_t.setHours(init_time[0], init_time[1])
                         now_t.setDate(now_t.getDate() + 1)
@@ -33,20 +33,20 @@ const totalTime = (start, now_t, init_time, hours, weekend, process) => {
                             now_t.setDate(now_t.getDate() + 1) 
                         }
 
-                        while(left_over > hours[0] + hours[1]) {
+                        while(left_over > hours[0] + hours[1]/10) {
                             console.log("i")
                             if(weekend.includes(now_t.getDay())) {
                                 now_t.setDate(now_t.getDate() + 1) 
                             } else {
-                                left_over -= (hours[0] + hours[1])
+                                left_over -= (hours[0] + hours[1]/10)
                                 now_t.setDate(now_t.getDate() + 1)
                             }
                         }
-                        now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*60)
+                        now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*6)
                         process[ptr].deadline = new Date(Date.parse(now_t))
                     }
                 } else {
-                    if(now_t.getHours() + now_t.getMinutes()/60 > (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) {
+                    if(now_t.getHours() + now_t.getMinutes()/60 > (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) {
                         now_t.setDate(now_t.getDate() + 1)
                     }
                     now_t.setHours(init_time[0], init_time[1])
@@ -55,15 +55,15 @@ const totalTime = (start, now_t, init_time, hours, weekend, process) => {
                     }
                     process[ptr].init_time = new Date(Date.parse(now_t))
                     var left_over = process[ptr].time_req/(1000*60*60)
-                    while(left_over > hours[0] + hours[1]) {
+                    while(left_over > hours[0] + hours[1]/10) {
                         if(weekend.includes(now_t.getDay())) {
                             now_t.setDate(now_t.getDate() + 1) 
                         } else {
-                            left_over -= (hours[0] + hours[1])
+                            left_over -= (hours[0] + hours[1]/10)
                             now_t.setDate(now_t.getDate() + 1)
                         }
                     }
-                    now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*60)
+                    now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*6)
                     process[ptr].deadline = new Date(Date.parse(now_t))
                 }
             }
--- route/user/project.js
+++ route/user/project.js
@@ -41,15 +41,15 @@ router.get('/', isUser, async (req, res)=>{
                     now_t.setDate(now_t.getDate() + 1)
                 }
                 console.log(now_t.getHours(), now_t.getDate())
-                if(now_t.getHours() + now_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && now_t.getHours() + now_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) {
+                if(now_t.getHours() + now_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && now_t.getHours() + now_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) {
                     element.init_time = new Date(Date.parse(now_t))
                     var after_t = new Date(Date.parse(now_t) + element.time_req)
-                    if((after_t.getHours() + after_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && after_t.getHours() + after_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) && now_t.getDate() == after_t.getDate() && now_t.getFullYear() == after_t.getFullYear() && now_t.getMonth() == after_t.getMonth()) {
+                    if((after_t.getHours() + after_t.getMinutes()/60 >= init_time[0] + init_time[1]/60 && after_t.getHours() + after_t.getMinutes()/60 <= (init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) && now_t.getDate() == after_t.getDate() && now_t.getFullYear() == after_t.getFullYear() && now_t.getMonth() == after_t.getMonth()) {
                         let val = element.time_req/(1000*60*60)
-                        now_t.setHours(now_t.getHours() + Math.floor(val), now_t.getMinutes() + ((val*10)%10)*60) 
+                        now_t.setHours(now_t.getHours() + Math.floor(val), now_t.getMinutes() + ((val*10)%10)*6) 
                         element.deadline = new Date(Date.parse(now_t))
                     } else {
-                        let dis = ((init_time[0] + hours[0]) + (init_time[1]/60 + hours[1])) - (now_t.getHours() + now_t.getMinutes()/60)
+                        let dis = ((init_time[0] + hours[0]) + (init_time[1]/60 + hours[1]/10)) - (now_t.getHours() + now_t.getMinutes()/60)
                         var left_over = element.time_req/(1000*60*60) - dis
                         now_t.setHours(init_time[0], init_time[1])
                         now_t.setDate(now_t.getDate() + 1)
@@ -58,16 +58,16 @@ router.get('/', isUser, async (req, res)=>{
                             now_t.setDate(now_t.getDate() + 1) 
                         }
 
-                        while(left_over > hours[0] + hours[1]) {
+                        while(left_over > hours[0] + hours[1]/10) {
                             console.log("i")
                             if(weekend.includes(now_t.getDay())) {
                                 now_t.setDate(now_t.getDate() + 1) 
                             } else {
-                                left_over -= (hours[0] + hours[1])
+                                left_over -= (hours[0] + hours[1]/10)
                                 now_t.setDate(now_t.getDate() + 1)
                             }
                         }
-                        now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*60)
+                        now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*6)
                         element.deadline = new Date(Date.parse(now_t))
                     }
                 } else {
@@ -80,15 +80,15 @@ router.get('/', isUser, async (req, res)=>{
                     }
                     element.init_time = new Date(Date.parse(now_t))
                     var left_over = element.time_req/(1000*60*60)
-                    while(left_over > hours[0] + hours[1]) {
+                    while(left_over > hours[0] + hours[1]/10) {
                         if(weekend.includes(now_t.getDay())) {
                             now_t.setDate(now_t.getDate() + 1) 
                         } else {
-                            left_over -= (hours[0] + hours[1])
+                            left_over -= (hours[0] + hours[1]/10)
                             now_t.setDate(now_t.getDate() + 1)
                         }
                     }
-                    now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*60)
+                    now_t.setHours(now_t.getHours() + Math.floor(left_over), now_t.getMinutes() + ((left_over*10)%10)*6)
                     element.deadline = new Date(Date.parse(now_t))
                 }
                 console.log(now_t.getHours(), now_t.getDate())
